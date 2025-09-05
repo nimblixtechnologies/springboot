61650f4a5ebbcf229a3c8bbfcdb521588093a842
ManishaBamin <manchuamin3@gmail.com>
2025-09-05 20:07:21 +0530

    "Manisha's first commit"

--- src/main/java/com/nimblix/springboot/git/Mygithub.java
+++ src/main/java/com/nimblix/springboot/git/Mygithub.java
@@ -2,7 +2,7 @@ package com.nimblix.springboot.git;
 
 public class Mygithub {
 	public static void main(String[] args) {
-		System.out.println("Hello Github");
+		System.out.println("Hello GitHub");
 	}
 	
 
