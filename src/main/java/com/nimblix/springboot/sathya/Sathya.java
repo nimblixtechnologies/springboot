af63ed20a287186977acf4c6541cd19a580b2d4c
Sathya <nsathyasaravanan@gmail.com>
2025-09-06 13:20:10 +0530

    First commit

---
+++ src/main/java/com/nimblix/springboot/sathya/Sathya.java
@@ -0,0 +1,9 @@
+package com.nimblix.springboot.sathya;
+
+public class Sathya {
+
+	public static void main(String[] args) {
+		System.out.println("Spring boot");
+	}
+	
+}
---
+++ src/main/java/com/nimblix/springboot/sathya/package-info.java
@@ -0,0 +1,1 @@
+package com.nimblix.springboot.sathya;
