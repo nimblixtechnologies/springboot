4a010f75fca54a4b45fc0db863dc5987c7556770
Sandeep-tech1 <tech.sandeep18@outlook.com>
2025-09-12 18:05:23 +0530

    Change m1() method to sandeepMethod() method

--- src/main/java/com/nimblix/springboot/sandeep/Sandeep.java
+++ src/main/java/com/nimblix/springboot/sandeep/Sandeep.java
@@ -1,7 +1,7 @@
 package com.nimblix.springboot.sandeep;
 
 public class Sandeep {
-	public void m1() {
-		System.out.println("gfyguydgudfg");
+	public void sandeepMethod() {
+		System.out.println("");
 	}
 }
